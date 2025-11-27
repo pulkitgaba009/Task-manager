8644c5e1292c3074851a4d65261d572a69a95604
pulkitgaba009 <pulkitgaba009@gmail.com>
2025-11-27 23:22:30 +0530

    middlewares added - rate limit using redis

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,18 +2,26 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app =  express();
 
-connectDB()
-
 // middleware
-app.use(express.json())
+app.use(express.json()); // this middleware will parse JSON body
+
+app.use(rateLimiter);
+
+// app.use((req,res,next)=>{   // Custom middleware  
+//     console.log(`Requested method ${req.method} & Req URL ${req.url}`);
+//     next();
+// })
 
 app.use("/api/notes",notesRoutes);
 
-app.listen(process.env.PORT || 3000,()=>{
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 3000,()=>{
     console.log("App is working...")
+})
 })
